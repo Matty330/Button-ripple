1bbc5417f9f8e5958d5931c9dc03c5da556d8c93
matt330 <mwelc64@yahoo.com>
2024-11-05 06:42:30 -0500

    Created buttone ripple

---
+++ java.js
@@ -0,0 +1,11 @@
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("mouseover", (event)=>{
+    const x = event.pageX - btnEl.offsetLeft;
+    const y = event.pageY - btnEl.offsettop;
+
+    btnEl.style.setProperty("--xPos", x + "px");
+    btnEl.style.setProperty("--yPos", y + "px");
+
+
+});
